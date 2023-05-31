bcdcfea68f248918bcc94f7cc2c18315466c2adc
Ahmadyama Qaderi <yamaahmad7@gmail.com>
2023-05-30 19:04:45 -0700

    updates add

--- src/main/java/tek/capstone/dragons/base/BaseSetup.java
+++ src/main/java/tek/capstone/dragons/base/BaseSetup.java
@@ -21,7 +21,7 @@ public class BaseSetup {
 	public BaseSetup() {
 
 		String filePath = System.getProperty("user.dir") + "/src/main/resources/env_config.yml";
-		String log4jPath = System.getProperty("user.dir") + "\\src\\main\\resources\\log4j.properties";
+		String log4jPath = System.getProperty("user.dir") + "/src/main/resources/log4j.properties";
 		try {
 			environmentVariables = ReadYamlFiles.getInstance(filePath);
 		} catch (Exception e) {
